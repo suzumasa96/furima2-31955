fc5f09053794fc1c8dd6dcbddd0c069b15fe298f
suzumasa96 <masakisuzuki.shkr@gmail.com>
2020-11-27 17:55:47 +0900

    rubocopにて修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,28 +1,28 @@
 class ItemsController < ApplicationController
-  #before_action :authenticate_user!
-  #before_action :move_to_index, except: [:index, :show]
+  # before_action :authenticate_user!
+  # before_action :move_to_index, except: [:index, :show]
 
   def index
   end
 
-  #def create
-    #@item = Item.new(item_prams)
-    #if @item.save
-      #redirect_to root_path
-    #else
-      #render new
-    #end
-  #end
+  # def create
+  # @item = Item.new(item_prams)
+  # if @item.save
+  # redirect_to root_path
+  # else
+  # render new
+  # end
+  # end
 
-  #private
-  #def item_prams
-    #params.requiere(:item).permit(:email,:password)
-    #.merge(user_id: current_user.id)
-  #end
+  # private
+  # def item_prams
+  # params.requiere(:item).permit(:email,:password)
+  # .merge(user_id: current_user.id)
+  # end
 
-  #def move_to_index
-    #unless user_signed_in?
-      #redirect_to action: :index
-    #end
-  #end
+  # def move_to_index
+  # unless user_signed_in?
+  # redirect_to action: :index
+  # end
+  # end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,48 +6,48 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
-    it "nameが空だと登録できない" do
-      @user.name = ""
+    it 'nameが空だと登録できない' do
+      @user.name = ''
       @user.valid?
       expect(@user.errors.full_messages).to include
     end
-    it "emailが空では登録できない" do
-      @user.email = ""
+    it 'emailが空では登録できない' do
+      @user.email = ''
       @user.valid?
       expect(@user.errors.full_messages).to include
     end
-    it "passwordが空では登録できない" do
-      @user.password = ""
+    it 'passwordが空では登録できない' do
+      @user.password = ''
       @user.valid?
       expect(@user.errors.full_messages).to include
     end
-    it "passwordは6文字以上で登録できる" do
-      @user.password = "000000"
-      @user.password_confirmation = "000000"
+    it 'passwordは6文字以上で登録できる' do
+      @user.password = '000000'
+      @user.password_confirmation = '000000'
       expect(@user).to be_valid
     end
-    it "last_nameが空では登録できない" do
-      @user.last_name = ""
+    it 'last_nameが空では登録できない' do
+      @user.last_name = ''
       @user.valid?
       expect(@user.errors.full_messages).to include
     end
-    it "last_name_kanaが空では登録できない" do
-      @user.last_name_kana = ""
+    it 'last_name_kanaが空では登録できない' do
+      @user.last_name_kana = ''
       @user.valid?
       expect(@user.errors.full_messages).to include
     end
-    it "first_nameが空では登録できない" do
-      @user.email = ""
+    it 'first_nameが空では登録できない' do
+      @user.email = ''
       @user.valid?
       expect(@user.errors.full_messages).to include
     end
-    it "first_name_kanaが空では登録できない" do
-      @user.email = ""
+    it 'first_name_kanaが空では登録できない' do
+      @user.email = ''
       @user.valid?
       expect(@user.errors.full_messages).to include
     end
-    it "birthdayが空では登録できない" do
-      @user.birthday = ""
+    it 'birthdayが空では登録できない' do
+      @user.birthday = ''
       @user.valid?
       expect(@user.errors.full_messages).to include
     end
